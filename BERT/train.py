d242acddda5d4e858a89b453666785e95946f8fd
michalbaldyga <michal.baldyga01@gmail.com>
2023-03-26 18:18:45 +0200

    Add dataset preprocess

---
+++ BERT/train.py
@@ -0,0 +1,18 @@
+from datasets import load_dataset
+from transformers import AutoTokenizer, DataCollatorWithPadding
+
+# Load dataset
+dataset = load_dataset("json", data_files="dataset.json")
+
+# Preprocess
+tokenizer = AutoTokenizer.from_pretrained("distilbert-base-uncased")
+
+
+def preprocess_function(examples):
+    """Preprocessing function to tokenize text and sequences"""
+    return tokenizer(examples["text"], truncation=True)
+
+
+tokenized_dataset = dataset.map(preprocess_function, batched=True)
+
+data_collator = DataCollatorWithPadding
