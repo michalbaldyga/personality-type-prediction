581aca7feb7c5cdfb1dcaf5321022ff8cbf5960f
Kacper Barański <102356251+BaranskiKacper@users.noreply.github.com>
2023-06-03 11:49:00 +0200

    Update clear.py

--- BERT/clear.py
+++ BERT/clear.py
@@ -4,7 +4,8 @@ import re
 def clear_data(text):
     # Removing Tweets
     text = re.sub(r'Tweets', '', text, re.DOTALL, flags=re.IGNORECASE)
-
+    # diffent emojis pattern
+    text = re.sub(r'\p{So}', '', string)
     # Emojis pattern
     text = re.sub("["
                   u"\U0001F600-\U0001F64F"  # emoticons
