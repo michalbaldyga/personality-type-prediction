e59e8ecc1fbca7fc49c006fa7f0d0c2e9004729d
michalbaldyga <michal.baldyga01@gmail.com>
2023-06-23 11:18:29 +0200

    change num_train_epochs

--- backend/train_model/train.py
+++ backend/train_model/train.py
@@ -17,7 +17,7 @@ def load_dataset_from_csv(path):
 
 
 # Load dataset and labels
-dataset = load_dataset_from_csv("../../datasets/dataset_twitter.csv")
+dataset = load_dataset_from_csv("../../datasets/tweets.csv")
 dataset = dataset.train_test_split(test_size=0.2)
 labels = ClassLabel(names_file='labels.txt')
 
@@ -67,7 +67,7 @@ training_args = TrainingArguments(
     learning_rate=2e-5,
     per_device_train_batch_size=16,
     per_device_eval_batch_size=16,
-    num_train_epochs=2,
+    num_train_epochs=5,
     weight_decay=0.01,
     evaluation_strategy="epoch",
     save_strategy="epoch",
