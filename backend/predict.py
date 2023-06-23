957bfeae15c61a1accd5c68285ee63211ebc4c3f
michalbaldyga <michal.baldyga01@gmail.com>
2023-06-23 11:16:41 +0200

    change accuracy measure

--- backend/predict.py
+++ backend/predict.py
@@ -25,17 +25,19 @@ def predict(text: str):
         batches = [text[i:i + batch_size] for i in range(0, len(text), batch_size)]
 
         # predict
-        score = 0.
         predictions = {}
         for batch in batches:
             result = classifier(batch, padding=True)
             for res in result[0]:
                 predictions[res['label']] = predictions.get(res['label'], 0) + res['score']
-                score += res['score']
 
         # get the best three as a list of personality types
         best_predictions = sorted(predictions.items(), key=lambda item: item[1], reverse=True)[0:3]
         best_predictions_list = []
+        score = 0.
+        for prediction in best_predictions:
+            score += prediction[1]
+
         for prediction in best_predictions:
             best_predictions_list.append({'label': str(prediction[0]),
                                           'percent': f"{round((prediction[1] / score) * 100, 2)}%"})
